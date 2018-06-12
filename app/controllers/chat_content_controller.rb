0503ae304582436a84c5bbf5ac356b166ad0a418
TeddyYin <ltboegod143@gmail.com>
2018-06-12 15:18:23 +0800

    show chat content

--- app/controllers/chat_content_controller.rb
+++ app/controllers/chat_content_controller.rb
@@ -4,5 +4,11 @@ class ChatContentController < ApplicationController
   end
 
   def show
+    @channel_id = params[:id]
+    received = Received.where(channel_id: @channel_id)
+    reply = Reply.where(channel_id: @channel_id)
+
+    @channel_chat = received + reply
+    @channel_chat.sort_by!{ |content| [content.created_at]}
   end
 end
