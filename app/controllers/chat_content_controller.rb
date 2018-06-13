3a21035598ec991dae95ad72f79163fe15ed5b0b
TeddyYin <ltboegod143@gmail.com>
2018-06-13 16:01:39 +0800

    change display channel id to channel name

--- app/controllers/chat_content_controller.rb
+++ app/controllers/chat_content_controller.rb
@@ -9,9 +9,11 @@ class ChatContentController < ApplicationController
   end
 
   def show
-    @channel_id = params[:id]
-    received = Received.where(channel_id: @channel_id)
-    reply = Reply.where(channel_id: @channel_id)
+    channel_id = params[:id]
+    @channel = Channel.find_by(channel_id: channel_id)
+
+    received = Received.where(channel_id: channel_id)
+    reply = Reply.where(channel_id: channel_id)
 
     @channel_chat = received + reply
     @channel_chat.sort_by!{ |content| [content.created_at]}
