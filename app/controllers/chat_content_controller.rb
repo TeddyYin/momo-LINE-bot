feb5d611f72766be8bd186bf962842a228716f4d
TeddyYin <ltboegod143@gmail.com>
2018-06-13 14:48:41 +0800

    add chat content name col

--- app/controllers/chat_content_controller.rb
+++ app/controllers/chat_content_controller.rb
@@ -1,6 +1,11 @@
 class ChatContentController < ApplicationController
   def index
     @channels = Channel.all.paginate(:page => params[:page], :per_page => 5)
+    @channels.each do |c|
+      Rails.logger.debug("channel_id   = #{c.channel_id}")
+      Rails.logger.debug("channel_name = #{c.channel_name}")
+      Rails.logger.debug("================================")
+    end
   end
 
   def show
